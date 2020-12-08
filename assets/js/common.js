ca461644a8cf4adc0c1bbda580a4dd438f29d943
Jiyun-Jeong <61535496+Jiyun-Jeong@users.noreply.github.com>
2020-12-08 13:59:57 +0900

    중간점검
    index2까지

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,3 +1,23 @@
 $(document).ready(function (){  
-    
+    //pc
+    //1) #pcGnb 네비
+    var _pcGnb = $('#pcGnb > ul');
+
+    _pcGnb.find('.dep2wrap').hide();
+    _pcGnb.find('> li > a').on('mouseenter focus', function () {
+        console.log('_pcGnb');
+        _pcGnb.find('>li.on').removeClass('on').children('.dep2wrap').hide();
+        $(this).next().show().parent().addClass('on');
+    });
+    _pcGnb.on('mouseleave', function () {
+        $(this).find('>li.on').removeClass('on').children('.dep2wrap').hide();
+    });
+
+    _pcGnb.find('a:first, a:last').on('blur', function () {
+        setTimeout(function () {
+            if( !$('#pcGnb a').is(':focus') ) _pcGnb.mouseleave();
+        }, 10);
+    });
+
+
 });
